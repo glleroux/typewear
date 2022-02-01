3c9252630b92c8761a3f2da372f2e483103b85ec
Rory Buckle <rorytbuckle@gmail.com>
2022-02-01 13:45:45 -0600

    added stripe public key

--- client/src/Components/StripeContainer.js
+++ client/src/Components/StripeContainer.js
@@ -3,7 +3,7 @@ import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
 import PaymentForm from './PaymentForm'
 
-const PUBLIC_KEY = 'pk_test_51Iz0OeAlXiPETQsLSE8tE9oogXXZgUMb5tFtVSZthbtC1vsVHQDXMowp1BV6ywNQlbCYzmEBykTUQ5amUfLNiwWb008Ul7KLHd'
+const PUBLIC_KEY = 'pk_live_51Iz0OeAlXiPETQsLoDSQMPHF7DxgFhSepMOxZHFA1kxZ5UwZbbdMLPH1xfTqtPt61ab7UUmY9W7VLYfZlSzxkTVD00Zz9hH0cv'
 const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
 const StripeContainer = ({setFormStep, order}) => {
